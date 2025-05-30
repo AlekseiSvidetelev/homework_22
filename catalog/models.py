a775ef953cab71022e2868d389444fd2e53ae1bc
Aleksei <svid.aleksei@ya.ru>
2025-05-30 15:27:01 +0300

    update project

--- catalog/models.py
+++ catalog/models.py
@@ -3,9 +3,15 @@ from unicodedata import category
 
 
 class Category(models.Model):
-    """ Категория товара """
+    """Категория товара"""
+
     name = models.CharField(max_length=100, verbose_name="Наименование")
-    description = models.TextField(verbose_name="Описание", help_text="Введите описание категории", blank=True, null=True)
+    description = models.TextField(
+        verbose_name="Описание",
+        help_text="Введите описание категории",
+        blank=True,
+        null=True,
+    )
 
     class Meta:
         verbose_name = "Категория"
@@ -16,14 +22,36 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    """ Товар """
-    name = models.CharField(max_length=100, verbose_name="Наименование", help_text="Введите наименование продукта")
-    description = models.TextField(verbose_name="Описание", help_text="Введите описание продукта")
-    photo = models.ImageField(upload_to="products/photo", blank=True, null=True, verbose_name="Изображение", help_text="Загрузите изображение продукта")
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products", verbose_name="Категория")
-    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена за покупку")
+    """Товар"""
+
+    name = models.CharField(
+        max_length=100,
+        verbose_name="Наименование",
+        help_text="Введите наименование продукта",
+    )
+    description = models.TextField(
+        verbose_name="Описание", help_text="Введите описание продукта"
+    )
+    photo = models.ImageField(
+        upload_to="products/photo",
+        blank=True,
+        null=True,
+        verbose_name="Изображение",
+        help_text="Загрузите изображение продукта",
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name="products",
+        verbose_name="Категория",
+    )
+    price = models.DecimalField(
+        max_digits=10, decimal_places=2, verbose_name="Цена за покупку"
+    )
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
-    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата последнего изменения")
+    updated_at = models.DateTimeField(
+        auto_now=True, verbose_name="Дата последнего изменения"
+    )
 
     class Meta:
         verbose_name = "Продукт"
